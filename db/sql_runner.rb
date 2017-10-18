c7c83117a9cfca4d3712df132cb72fc51804015d
Callum <callumpb@hotmail.com>
2017-10-18 11:48:55 +0100

    sql_runner added

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,16 +3,8 @@ require('pg')
 class SqlRunner
 
   def self.run ( sql, values )
-  end
-
-
-
-require('pg')
-class SqlRunner
-
-  def self.run( sql, values )
     begin
-      db = PG.connect({ dbname: 'pizza_shop', host: 'localhost'})
+      db = PG.connect({ dbname: 'hogwarts', host 'localhost'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
@@ -20,5 +12,4 @@ class SqlRunner
     end
     return result
   end
-
 end
